c4e537de0fea9418989d2ab5ca7b43baa81905bc
Gustavo Almeida <gustavo.almeida@live.com>
2018-01-17 17:35:08 -0200

    Inclusão de cenário ValidarOpcaoSairQueEncerraAAplicacao

--- src/test/java/Cpav.java
+++ src/test/java/Cpav.java
@@ -113,4 +113,47 @@ public class Cpav extends Base {
 
         Assert.assertTrue(driver.findElementByAccessibilityId("Valor de entrada:").isDisplayed());
     }
+
+    @Test
+    public void ValidarOpcaoSairQueEncerraAAplicacao(){
+        driver.findElementByName("ACESSE SUA CONTA").click();
+        driver.findElementByClassName("XCUIElementTypeTextField").sendKeys("juliano");
+        driver.findElementByName("CONTINUAR").click();
+        driver.findElementByClassName("XCUIElementTypeTextField").sendKeys("590");
+        driver.findElementByName("CONTINUAR").click();
+        driver.findElementByClassName("XCUIElementTypeTextField").sendKeys("92819");
+        driver.findElementByName("CONTINUAR").click();
+        driver.findElementByClassName("XCUIElementTypeSecureTextField").sendKeys("1122");
+        driver.findElementByName("CONTINUAR").click();
+        driver.findElementByName("CONTINUAR").click();
+
+        IOSElement banner = driver.findElementByXPath("//*/XCUIElementTypeCell/XCUIElementTypeOther[2]");
+
+        //Coordenadas da posição do elemento
+        int x_element = banner.getLocation().getX();
+        int y_element = banner.getLocation().getY();
+
+        //Coordenadas do toque inicial (press())
+        int x_inicial = banner.getSize().getWidth() - x_element;
+        int y_inicial = banner.getSize().getHeight() + y_element;
+
+        //Coordenadas do toque final (moveTo())
+        int x_final = x_inicial - (x_inicial + 300);
+        int y_final = 0;
+
+        //Swipe to Delete
+        (new TouchAction(driver))
+                .press(x_inicial, y_inicial)
+                .moveTo(x_final, y_final)
+                .release()
+                .perform();
+
+        driver.findElementByAccessibilityId("fun-credit-offer-vehicles").click();
+        driver.findElementByAccessibilityId("Solicitar").click();
+
+        driver.findElementByName("menu").click();
+        driver.findElementByName("logout").click();
+        driver.findElementByName("Sair").click();
+        Assert.assertTrue(driver.findElementByAccessibilityId("iphone-logo").isDisplayed());
+    }
 }
